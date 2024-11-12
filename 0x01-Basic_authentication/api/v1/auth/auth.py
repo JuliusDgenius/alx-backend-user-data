fd647b54414b05cd887e08bb5fb7472bec140fb8
JuliusDgenius <ibejulius1@gmail.com>
2024-11-12 23:02:59 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-"""Module defines the Auth class"""
+""" API authentication
+"""
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
-    """class Auth to manage API authentication"""
-
+class Auth():
+    """ manages the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns a Bool"""
+        """ require authorithation check"""
         if path is None or excluded_paths is None or not len(excluded_paths):
-        	return True
+            return True
         if path[-1] != '/':
             path += '/'
         for p in excluded_paths:
             if p.endswith('*'):
-                if p.startswith(p[:1]):
+                if path.startswith(p[:1]):
                     return False
         return False if path in excluded_paths else True
 
-
     def authorization_header(self, request=None) -> str:
-        """Returns authorization header"""
+        """ authorization header check"""
         if request:
-        	return request.headers.get('Authorization')
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Returns the current user"""
+        """ current user method"""
         return None
