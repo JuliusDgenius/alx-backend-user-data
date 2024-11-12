594995bc2c56b0df76bc79928244f6cc499bf8c7
JuliusDgenius <ibejulius1@gmail.com>
2024-11-12 22:38:40 +0100

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,12 +6,19 @@ from typing import List, TypeVar
 
 class Auth:
     """class Auth to manage API authentication"""
-    def __init__(self) -> str:
-        """Initialize the Auth object"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns a Bool"""
-        return False
+        if path is None or excluded_paths is None or not len(excluded_paths):
+        	return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if p.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
+
 
     def authorization_header(self, request=None) -> str:
         """Returns authorization header"""
