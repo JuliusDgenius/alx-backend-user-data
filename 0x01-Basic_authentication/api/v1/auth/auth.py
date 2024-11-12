176f74addb5994377f039191f678e58a18dc1be8
JuliusDgenius <ibejulius1@gmail.com>
2024-11-12 21:58:21 +0100

    task

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module defines the Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+	"""class Auth to manage API authentication"""
+	def __init__(self) -> str:
+		"""Initialize the Auth object"""
+
+	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+		"""Returns a Bool"""
+		return False
+
+	def authorization_header(self, request=None) -> str:
+		return None
+
+	def current_user(self, request=None) -> TypeVar('User'):
+		return None
