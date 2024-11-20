fd8f6deafc2bd1190f45b9dd539c945a7ae8e1dd
JuliusDgenius <ibejulius1@gmail.com>
2024-11-20 10:47:39 +0100

    D.update_user: Implement the update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -70,11 +70,3 @@ class DB:
                 self.__session.commit()
             except ValueError:
                 print('user has no attribute', key)
-
-          # user = self.find_user_by(id=user_id)
-          # for key, val in kwargs.items():
-          #   if key not in DATA:
-          #       raise ValueError
-          #   setattr(user, key, val)
-          #   self._session.commit()
-          #   return None
