5a0ce147aef037e2c319fe5728a7ecba8096b992
JuliusDgenius <ibejulius1@gmail.com>
2024-11-20 07:39:01 +0100

    D.find_user: Implement the find_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs: dict)-> User:
+    def find_user_by(self, **kwargs)-> User:
         """
         Returns the first row found in the `users` table.
         """
